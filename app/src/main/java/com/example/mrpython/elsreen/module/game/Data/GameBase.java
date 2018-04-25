0a1eaea66310d7db48e671a828c3d350011a5018
nhan14t1 <nhan.14t1@gmail.com>
2018-04-25 20:15:04 +0700

    aaa

--- app/src/main/java/com/example/mrpython/elsreen/module/game/Data/GameBase.java
+++ app/src/main/java/com/example/mrpython/elsreen/module/game/Data/GameBase.java
@@ -1,27 +1,139 @@
 package com.example.mrpython.elsreen.module.game.Data;
 
+import android.content.Context;
+
+import com.example.mrpython.elsreen.MainActivity;
+import com.example.mrpython.elsreen.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by vuong on 3/25/2018.
  */
 
 public class GameBase {
     private Player player;
-    private Question question;
-    public GameBase(){
-        this.player = new Player();
-        this.question = new Question();
-        setData();
+    private ArrayList<Question> listQuestion;
+    private Context context;
+    private int resourceDataId = R.raw.data;
+    private static GameBase gameBase;
+
+    private GameBase(Context context) {
+        this.setContext(context);
+        this.player = new Player(context);
+        listQuestion = new ArrayList<>();
+        this.setData();
     }
-    public void setData(){
-        this.player.cheatData();
-        this.question.cheatData();
+
+    public static GameBase getGameBase(Context context)
+    {
+        if (gameBase  == null)
+            gameBase = new GameBase(context);
+        else
+            gameBase.setContext(context);
+        return gameBase;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+//        this.player.setContext(context);
+    }
+
+    public void setData() {
+        this.player.loadData();
+        try {
+            this.readJsonDataFile();
+        } catch (Exception e) {
+            System.out.println("ERROR:" + e.getMessage());
+        }
     }
     //region GET
-    public Player getPlayer(){
+    public Player getPlayer() {
         return this.player;
     }
-    public Question getQuestion(){
-        return this.question;
+
+    public Question getRandomQuestion() {
+        Random rand = new Random();
+        int index = rand.nextInt(this.listQuestion.size());
+
+        return this.listQuestion.get(index);
     }
-    //endregion
+
+    public void readJsonDataFile() throws IOException, JSONException {
+
+        // read data on raw/data.json
+        String data = readFile(this.context, this.resourceDataId);
+        JSONArray jsonArrData = new JSONArray(data);
+
+        for (int i = 0; i < jsonArrData.length(); i++) {
+            JSONObject jsonData = jsonArrData.getJSONObject(i);
+
+            int id = jsonData.getInt("id");
+            String content = jsonData.getString("content");
+
+            JSONArray arrAnswer = jsonData.getJSONArray("answers");
+            ArrayList<String> answers = new ArrayList<>();
+
+            for (int k = 0; k < arrAnswer.length(); k++) {
+                answers.add(arrAnswer.getString(k));
+            }
+
+            String result = jsonData.getString("result");
+            Question question = new Question();
+            question.setData(content, result, answers);
+
+            this.listQuestion.add(question);
+        }
+    }
+
+    private String readFile(Context context, int resId) throws IOException {
+        InputStream in = context.getResources().openRawResource(resId);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringBuilder sb = new StringBuilder();
+        String s = null;
+        while((s = br.readLine()) != null) {
+            sb.append(s);
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    //Tăng kinh nghiệm, trả lời đúng 1 câu được tăng 1 điểm kinh nghiệm
+    //Trả lời đúng 3 câu liên tiếp tăng 2 điểm kinh nghiệm.
+    //Trả lời đúng 5 câu liên tiếp tăng 3 điểm kinh nghiệm
+    //Hiện đang để tăng 4 điểm để test
+    public void expUp(int numberOfTrueAnswers)
+    {
+        int point;
+        if (numberOfTrueAnswers < 3)
+            point = 4;
+        else
+            if (numberOfTrueAnswers < 5)
+                point = 2;
+            else
+                point = 3;
+        player.setCurExp(player.getCurExp() + point);
+
+        checkLevelUp();
+    }
+
+    private void checkLevelUp()
+    {
+        if (player.getCurExp() > 9)
+        {
+            player.setLevel(player.getLevel() + 1);
+            player.setCurExp(player.getCurExp()%10);
+            player.saveData();
+        }
+    }
 }
--- app/src/main/java/com/example/mrpython/elsreen/module/game/Data/Player.java
+++ app/src/main/java/com/example/mrpython/elsreen/module/game/Data/Player.java
@@ -1,60 +1,77 @@
 package com.example.mrpython.elsreen.module.game.Data;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by vuong on 3/25/2018.
  */
 
 public class Player {
     private String uName;
-    private  long scores;
+    private long scores;
     private int level;
     private long curExp;
+    private Context context;
 
-    public  Player(){
-
+    public  Player(Context context){
+        this.context = context;
+        scores = 0;
+        level = 1;
+        curExp = 0;
     }
 
     //region SET && GET
-    public void setName(String uName){
+    public void setName(String uName) {
         if(uName == null) return;
         this.uName = uName;
     }
-    public void setScores(long scores){
+
+    public void setScores(long scores) {
         if(scores < 0) return;
         this.scores = scores;
     }
-    public void setLevel(int level){
+
+    public void setLevel(int level) {
         if(level < 0) return;
         this.level = level;
     }
-    public void setCurExp(long curExp){
+
+    public void setCurExp(long curExp) {
         if(curExp < 0) return;
         this.curExp = curExp;
     }
-    public String getName(){
+
+    public void setContext(Context context)
+    {
+        this.context = context;
+    }
+
+    public String getName() {
         if(this.uName == null) return "User";
         return this.uName;
     }
-    public long getScores(){
+
+    public long getScores() {
         return this.scores;
     }
-    public  int getLevel(){
+    public  int getLevel() {
         return this.level;
     }
-    public long getCurExp(){
+    public long getCurExp() {
         return this.curExp;
     }
     // endregion
 
-    public void addScore(long s){
+    public void addScore(long s) {
         if(s <= 0 ) return;
         this.scores += s;
     }
-    public void addCurExp(long e){
+    public void addCurExp(long e) {
         if(e <=0 ) return;
         this.curExp += e;
     }
-    public void addLevel(int l){
+    public void addLevel(int l) {
         if(l <= 0) {
             this.level ++;
         }else{
@@ -62,17 +79,32 @@ public class Player {
         }
 
     }
-    public void setData(String uName, long scores, int level, long curExp){
+
+    public void setData(String uName, long scores, int level, long curExp) {
         this.setName(uName);
         this.setScores(scores);
         this.setLevel(level);
         this.setCurExp(curExp);
     }
-    public void cheatData(){
-        String name = "Mr Python";
+
+
+    public void loadData()
+    {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("gameData", Context.MODE_PRIVATE);
+
+        this.uName = sharedPreferences.getString("name", "");
+        this.level = sharedPreferences.getInt("level", 1);
+        this.curExp = sharedPreferences.getLong("exp", 0);
         long scores = 100;
-        int level = 3;
-        long curExp = 123;
-        this.setData(name, scores, level, curExp);
+    }
+
+    public void saveData()
+    {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("gameData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("name", this.uName);
+        editor.putInt("level", this.level);
+        editor.putLong("exp", this.curExp);
+        editor.apply();
     }
 }
